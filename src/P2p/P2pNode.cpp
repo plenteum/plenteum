6b38c003b9d0c26f1942e269bb51f98f16b0ce8f
WhassupZA <support@plenteum.com>
2018-08-02 21:52:21 +0200

    licensing fix

--- src/P2p/P2pNode.cpp
+++ src/P2p/P2pNode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers, The Plenteum developers
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers, The TurtleCoin developers, , The Plenteum developers
 //
 // This file is part of Bytecoin.
 //
