b07a63910b6ea49b2cbd0209a6ac112eed9030bc
Dave <david@itbsoftware.co.za>
2018-10-28 10:49:34 +0200

    Bump p2p versions

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -202,8 +202,8 @@ const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
 
 // P2P Network Configuration Section - This defines our current P2P network version
 // and the minimum version for communication between nodes
-const uint8_t  P2P_CURRENT_VERSION                           = 2;
-const uint8_t  P2P_MINIMUM_VERSION                           = 1;
+const uint8_t  P2P_CURRENT_VERSION                           = 3;
+const uint8_t  P2P_MINIMUM_VERSION                           = 2; //bumped to min supported software version for peer connect
 // This defines the number of versions ahead we must see peers before we start displaying
 // warning messages that we need to upgrade our software.
 
